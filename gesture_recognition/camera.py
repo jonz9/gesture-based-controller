74cd9515f299e22a6e60509b49e5ba91547bf878
John Zhang <j444zhan@uwaterloo.ca>
2025-03-31 23:55:17 -0600

    hand sign classification

--- gesture_recognition/camera.py
+++ gesture_recognition/camera.py
@@ -1,5 +1,6 @@
 from collections import deque
 import csv
+import copy
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
@@ -119,15 +120,55 @@ def main(cap_device, cap_width, cap_height, max_num_hands, use_static_image_mode
      return landmark_point
   
 
-  def preprocess_landmark(landmark_list):
+  def pre_process_landmark(landmark_list):
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+      
 
   # Main loop
   while True:
     fps = cvFpsCalc.get()
 
-    # Process Key (ESC: end)
+    # ESC: end key
     key = cv.waitKey(10)
-    if key == 27: # ESC
+    if key == 27: 
       break
     number, mode = select_mode(key, mode)
 
+    # capture frames from camera
+    ret, image = cap.read()
+    if not ret:
+      break
+    image = cv.flip(image, 1)
+    debug_image = copy.deepcopy(image)
+
+    # detection
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = hands.process(image)
+    image.flags.writeable = True
+
+    # draw landmarks if detected
+    if results.multi_hand_landmarks is not None:
+      for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+        brect = calc_bounding_rect(debug_image, hand_landmarks)
+        landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+
+        pre_processed_landmark_list = pre_process_landmark(landmark_list)
+        pre_processed_point_history_list = pre_process_point_history(point_history)
+
+        # write to dataset
+        logging_csv(number, mode, pre_processed_landmark_list, pre_processed_point_history_list)
+  
+        # hand sign classification
+        hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+        if hand_sign_id == 2:
+          point_history.append(landmark_list[8])
+        else:
+          point_history.append([0, 0])
+
+
+        
